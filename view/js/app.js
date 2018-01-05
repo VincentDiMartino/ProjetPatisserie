a2149dbfca0f15683583e79f5c233df8f4d94a75
Théo Delory <theo.delory@gmail.com>
2018-01-05 11:18:08 +0100

    test

--- view/js/app.js
+++ view/js/app.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
-  $("#searchInput").on("keyup", function() {
+$(document).ready(function () {
+  $("#searchInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#productsTable tr").filter(function() {
+    $("#productsTable tr").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
   $("#productsTable").stupidtable();
 });
+
+// hey
